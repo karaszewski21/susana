328ffc926c6370b1b8f19158139908903319d047
karaszewski21 <pkaraszewski@agora.pl>
2022-03-15 18:03:49 +0100

    first commit

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -1,10 +1,10 @@
 export default defineNuxtRouteMiddleware((to, from) => {
-  console.log('middle');
-  const cookie = useCookie('user_id');
-  if (!cookie.value) {
-    if (to.path.includes('/login')) {
-      return;
-    }
-    return navigateTo('/login');
-  }
+  // console.log('middle');
+  // const cookie = useCookie('user_id');
+  // if (!cookie.value) {
+  //   if (to.path.includes('/login')) {
+  //     return;
+  //   }
+  //   return navigateTo('/login');
+  // }
 });
