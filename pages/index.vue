d20465ca4250c1160ea308eb70fa72c02ff75a88
karaszewski21 <pkaraszewski@agora.pl>
2022-03-24 17:39:00 +0100

    add turn

--- pages/index.vue
+++ pages/index.vue
@@ -48,17 +48,7 @@ const iceServers = {
     { urls: 'stun:stun3.l.google.com:19302' },
     { urls: 'stun:stun4.l.google.com:19302' },
     {
-      urls: 'turn:numb.viagenie.ca',
-      credential: 'muazkh',
-      username: 'webrtc@live.com',
-    },
-    {
-      urls: 'turn:turn.anyfirewall.com:443?transport=tcp',
-      credential: 'webrtc',
-      username: 'webrtc',
-    },
-    {
-      urls: 'turn:192.158.29.39:3478?transport=tcp',
+      urls: 'turn:192.158.29.39:3478?transport=udp',
       credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
       username: '28224511:1379330808',
     },
